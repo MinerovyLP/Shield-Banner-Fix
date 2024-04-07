c6f1e77f263fa9592143c8a222fa2485a04ebdc0
Tyler Hancock <darklime@live.ca>
2024-04-07 02:00:46 -0600

    Port to MC 1.18.2

---
+++ Common/src/main/java/net/darkhax/shieldbannerfix/Constants.java
@@ -0,0 +1,11 @@
+package net.darkhax.shieldbannerfix;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "shieldbannerfix";
+    public static final String MOD_NAME = "ShieldBannerFix";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ Forge/src/main/java/net/darkhax/shieldbannerfix/ShieldBannerFixForge.java
@@ -0,0 +1,8 @@
+package net.darkhax.shieldbannerfix;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class ShieldBannerFixForge {
+
+}
